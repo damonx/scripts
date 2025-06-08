ec5cbf8192402f8fcdd07e462f0419349cd6d87f
Damon Xu <akldamonx@icloud.com>
2025-06-08 22:06:40 +1200

    new python examples

---
+++ python/dictionary.py
@@ -0,0 +1,6 @@
+friends_ages = {"Damon": 24, "Adam": 30, "Anne": 27}
+print(friends_ages["Damon"]);
+
+friends_ages["Bob"] = 20
+friends_ages["Damon"] = 49
+print(friends_ages)
---
+++ python/dump_db_pickle_recs.py
@@ -0,0 +1,12 @@
+import pickle, glob
+for filename in glob.glob('*.pkl'):
+    recfile = open(filename, 'rb')
+    record = pickle.load(recfile)
+    print(filename, '=>\n ', record)
+
+suefile = open('sue.pkl', 'rb')
+print(pickle.load(suefile)['name'])
+
+for filename in glob.glob('*.pkl'):
+    recfile = open(filename, 'rb')
+    print('Name', ':', pickle.load(recfile)['name'])

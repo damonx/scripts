0a467cf072f68158d7f53419fb22c83336a38f4a
Damon Xu <damon.xu@spark.co.nz>
2025-10-07 15:47:39 +1300

    new code

--- python/oop/alien-invasion/settings.py
+++ python/oop/alien-invasion/settings.py
@@ -18,5 +18,18 @@ class Settings:
         # Alien settings
         self.alien_speed = 1.0
         self.fleet_drop_speed = 10
+
+        # How quickly the game speeds up
+        self.speedup_scale = 1.1
+        self.initialize_dynamic_settings()
+
+    def initialize_dynamic_settings(self):
+        """Initialize the game settings."""
+        self.ship_speed = 1.5
+        self.bullet_speed = 2.5
+        self.alien_speed = 1.0
+
         # fleet_direction of 1 represents right; -1 represents left.
         self.fleet_direction = 1
+
+

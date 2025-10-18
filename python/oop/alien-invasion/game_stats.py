f1a91db76b04029c70c8f1ee0c529ee3209b53bb
Damon Xu <damon.xu@spark.co.nz>
2025-10-18 22:42:31 +1300

    Add high score feature

--- python/oop/alien-invasion/game_stats.py
+++ python/oop/alien-invasion/game_stats.py
@@ -5,6 +5,8 @@ class GameStats:
         """Initialize variables."""
         self.settings = ai_game.settings
         self.reset_stats()
+        # High score should new be reset.
+        self.high_score = 0
 
     def reset_stats(self):
         """Initialize statistics that can change during the game."""

8c7bc305fad4c6c0b167ba75f76c9b27c321bdd7
Damon Xu <damon.xu@spark.co.nz>
2025-12-19 22:57:18 +1300

    new

--- golang/struts/main.go
+++ golang/struts/main.go
@@ -2,15 +2,48 @@ package main
 
 import "fmt"
 
+type contactInfo struct {
+	email   string
+	zipCode int
+}
+
 type person struct {
 	firstName string
 	lastName  string
+	contactInfo
 }
 
 func main() {
 	p := person{firstName: "Alex", lastName: "Anderson"}
-	fmt.Println("p:", p)
+	p.print()
 
-	
+	var damon person
+	damon.firstName = "Damon"
+	damon.lastName = "Xu"
+	damon.contactInfo.email = "test@test.com"
+	damon.contactInfo.zipCode = 123456
+	damon.print()
 
+	var zoe person
+	zoe = person{
+		firstName: "Zoe",
+		lastName:  "Liang",
+		contactInfo: contactInfo{
+			email:   "zoe@test.com",
+			zipCode: 654321,
+		},
+	}
+	zoe.print()
+	// pPointer := &p
+	// pPointer.updateName("Elodie")
+	p.updateName("Elodie")
+	p.print()
+}
+
+func (p *person) updateName(newFirstName string) {
+	(*p).firstName = newFirstName
+}
+
+func (p person) print() {
+	fmt.Printf("%+v\n", p)
 }
--- python/oop/data/mpl_squares.py
+++ python/oop/data/mpl_squares.py
@@ -1,7 +1,17 @@
 import matplotlib.pyplot as plt
 
-squares = [1, 4, 9, 16, 25]
+x_values = range(1, 1001)
+y_values = [x**2 for x in x_values]
 
+plt.style.use("seaborn-v0_8-darkgrid")
 fig, ax = plt.subplots()
-ax.plot(squares)
+ax.scatter(x_values, y_values, c = y_values, cmap = plt.cm.Blues, s = 10)
+# ax.plot(input_values, squares, linewidth=3)
+ax.set_title("Square Numbers", fontsize=24)
+ax.set_xlabel("Value", fontsize=14)
+ax.set_ylabel("Square of Value", fontsize=14)
+ax.axis([0, 1100, 0, 1_100_000])
+ax.ticklabel_format(style='plain')
+
+plt.savefig('squares_plot.png', bbox_inches='tight')
 plt.show()

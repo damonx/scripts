e94a791b374e7d92a112ceb4b3fdfc40a6814ee4
Damon Xu <damon.xu@spark.co.nz>
2026-02-14 16:58:06 +1300

    new

---
+++ javascript/optionalpropertyaccess.js
@@ -0,0 +1,15 @@
+function city(object) {
+  return object.address?.city;
+}
+console.log(city({address: {city: "Toronto"}}));
+// → Toronto
+console.log(city({name: "Vera"}));
+// → undefined
+
+
+console.log("string".notAMethod?.());
+// → undefined
+console.log("string".slice?.(1));
+// tring
+console.log({}.arrayProp?.[0]);
+// → undefined

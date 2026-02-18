1d0df1efb2ceb0f2150d82a5ba5b056feec80171
Damon Xu <damon.xu@spark.co.nz>
2026-02-19 00:10:00 +1300

    abstraction

--- javascript/abstraction.js
+++ javascript/abstraction.js
@@ -10,3 +10,15 @@ hungryRabbit.speak("I could use a carrot right now.");
 
 //  The white rabbit says 'Oh my ears and whiskers, how late it's getting!'
 //  The hungry rabbit says 'I could use a carrot right now.'
+
+speak.call(hungryRabbit, "Burp!"); // The hungry rabbit says 'Burp!'
+speak.call(whiteRabbit, "Yowza!"); // The white rabbit says 'Yowza!'
+
+let finder = {
+  find(array) {
+    return array.some(v => v == this.value);
+  },
+  value: 5
+};
+console.log(finder.find([4, 5]));
+// → true

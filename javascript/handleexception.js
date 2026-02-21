92882ec76ecd357c9a96dd08f518a324073a644b
Damon Xu <damon.xu@spark.co.nz>
2026-02-21 23:14:19 +1300

    new

--- javascript/handleexception.js
+++ javascript/handleexception.js
@@ -18,3 +18,19 @@ try {
 } catch (error) {
   console.log("Something went wrong: " + error);
 }
+
+// 下面是一个使用 finally 来保证在发生错误时仍然能正确处理的例子
+function transfer(from, amount) {
+  if (accounts[from] < amount) return;
+  let progress = 0;
+  try {
+    accounts[from] -= amount;
+    progress = 1;
+    accounts[getAccount()] += amount;
+    progress = 2;
+  } finally {
+    if (progress == 1) {
+      accounts[from] += amount;
+    }
+  }
+}

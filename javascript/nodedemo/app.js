9674ce60f2239f9b0a32031ddb518ee1558576ea
Damon Xu <nzdamon@gmail.com>
2026-03-24 23:50:59 +1300

    new js

--- javascript/nodedemo/app.js
+++ javascript/nodedemo/app.js
@@ -1,8 +1,11 @@
 // A simple http server in Node.js
 import http from 'http';
+import fs from 'fs';
 
 const server = http.createServer((req, res) => {
-  const url = req.url;
+  // const url = req.url;
+  // const method = req.method;
+  const {url, method} = req;
   if (url === '/') {
     res.write('<html>');
     res.write('<head><title>Enger Message</title></head>');
@@ -10,7 +13,22 @@ const server = http.createServer((req, res) => {
     res.write('</html>');
     return res.end();
   }
-  console.log(req.url, req.method, req.headers);
+  if (url === '/message' && method === 'POST') {
+    const body = [];
+    req.on('data', (chunk) => {
+      console.log(chunk);
+      body.push(chunk); 
+    });
+    req.on('end', () => {
+      const parsedBody = Buffer.concat(body).toString();
+      const message = parsedBody.split('=')[1];
+      fs.writeFileSync('message.txt', message);
+      console.log("provided message: ", message);
+    });
+    res.writeHead(302, {'Location': '/'});
+    return res.end();
+  }
+  //console.log(req.url, req.method, req.headers);
   res.setHeader('Content-Type', 'text/html');
   res.end('<h1>Hey Damon, this is my first HTML page!</h1>');
 });
